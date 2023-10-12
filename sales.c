1d89de07364c03e96d5e0e6f2cfd39e5e3d125d8
morriswa <william@morriswa.org>
2023-10-12 11:11:21 -0500

    console output instead of file output, file input is okay

--- sales.c
+++ sales.c
@@ -42,9 +42,9 @@ void get_months_in_descending_order(double* sales_by_month, int* dest);
 /**
  * function that takes in a double[] of sales ordered by month
  *  AND an int[] array of months ordered by sales in descending order
- *  and creates an output file to save final sales report in
+ *  and prints the report to the console
 */
-void generate_sales_report_and_save(double* sales_ordered_by_month, int* max_to_min_sales_months);
+void print_sales_report(double* sales_ordered_by_month, int* max_to_min_sales_months);
 
 
 int main(int argc, char** argv) {
@@ -67,7 +67,7 @@ int main(int argc, char** argv) {
     get_months_in_descending_order(sales_ordered_by_month, max_to_min_sales_months);
   
     // now that we have sales by month and months by sale, generate the annual sales report
-    generate_sales_report_and_save(sales_ordered_by_month, max_to_min_sales_months);
+    print_sales_report(sales_ordered_by_month, max_to_min_sales_months);
 
     // exit wo errors :)
     return 0; 
@@ -108,27 +108,25 @@ void get_months_in_descending_order(double* sales_by_month, int* dest) {
     }
 }
 
-void generate_sales_report_and_save(double* sales_ordered_by_month, int* max_to_min_sales_months) {
-    // open an output file in WRITE mode
-    FILE* output_file = fopen("./sales_report.txt","w"); 
+void print_sales_report(double* sales_ordered_by_month, int* max_to_min_sales_months) {
 
-    fprintf(output_file, "\nMonthly sales report for 2022: \n\tMONTH\t\tSALES\n");
+    printf("\nMonthly sales report for 2022: \n\tMONTH\t\tSALES\n");
 
     // print out sales for each month in order by month
     for (int i = 0; i < 12; i++)
-        fprintf(output_file, "\t%s\t\t$%.2f\n",MONTHS[i],sales_ordered_by_month[i]);
+        printf("\t%s\t\t$%.2f\n",MONTHS[i],sales_ordered_by_month[i]);
 
 
-    fprintf(output_file, "\nSales summary: \n");
+    printf("\nSales summary: \n");
 
     // locate worst month for sales and output
-    fprintf(output_file, "\tMinimum sales:\t$%.2f (%s)\n", 
+    printf("\tMinimum sales:\t$%.2f (%s)\n", 
         // smallest sales month stored last in max->min month array 
         sales_ordered_by_month[max_to_min_sales_months[11]], 
         MONTHS[max_to_min_sales_months[11]]); 
 
     // locate best month for sales and output
-    fprintf(output_file, "\tMaximum sales:\t$%.2f (%s)\n", 
+    printf("\tMaximum sales:\t$%.2f (%s)\n", 
         // max sales month stored first in max->min month array
         sales_ordered_by_month[max_to_min_sales_months[0]], 
         MONTHS[max_to_min_sales_months[0]]);
@@ -142,9 +140,9 @@ void generate_sales_report_and_save(double* sales_ordered_by_month, int* max_to_
     // and divide by 12 to calculate average
     total_avg_sales /= 12;
     // and output
-    fprintf(output_file, "\tAverage sales:\t$%.2f\n", total_avg_sales);
+    printf("\tAverage sales:\t$%.2f\n", total_avg_sales);
     
-    fprintf(output_file, "\nSix-Month Moving Average Report: \n");
+    printf("\nSix-Month Moving Average Report: \n");
     for (int start_month = 0; start_month <= 6; start_month++) {
         int final_month = start_month + 5; 
 
@@ -155,20 +153,17 @@ void generate_sales_report_and_save(double* sales_ordered_by_month, int* max_to_
         }
         average_sales /= 6;
 
-        fprintf(output_file, "\t%s -> %s \t $%.2f\n",
+        printf("\t%s -> %s \t $%.2f\n",
             MONTHS[start_month],
             MONTHS[final_month],
             average_sales); 
     }
 
-    fprintf(output_file, "\nSales Report (Highest to Lowest):\n\tMONTH\t\tSALES\n");
+    printf("\nSales Report (Highest to Lowest):\n\tMONTH\t\tSALES\n");
     for (int i = 0; i < 12; i++) {
         // go through max->min array and print out sales figure for every month in order by sales.
-        fprintf(output_file, "\t%s\t\t$%.2f\n",
+        printf("\t%s\t\t$%.2f\n",
             MONTHS[max_to_min_sales_months[i]], 
             sales_ordered_by_month[max_to_min_sales_months[i]]);
     }
-
-    // close the output file, sales report generation is complete
-    fclose(output_file);
 }
